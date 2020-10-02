d4ebc7f6882f8723a80095058eaf209ccaa805b5
Aleksandra <aleksa@yandex.ru>
2020-10-02 12:01:31 +0300

    master решение задач 2 урока

---
+++ src/lesson2/task_dead_lock/DeadLock.java
@@ -0,0 +1,41 @@
+package lesson2.task_dead_lock;
+
+public class DeadLock {
+
+    static class Friend {
+        private final String name;
+
+        public Friend(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public synchronized void bow(Friend bower) {
+            System.out.format("%s: %s подстрелил меня!\n", this.name, bower.getName());
+            System.out.format("%s: стреляю в ответ!\n", this.name);
+            bower.bowBack(this);
+        }
+
+        public void bowBack(Friend bower) {
+            synchronized (bower) {
+                System.out.format("%s: %s подстрелил меня!\n", this.name, bower.getName());
+            }
+        }
+
+        /**
+         * Точка входа в программу
+         *
+         * @param args аргументы командной строки
+         */
+        public static void main(String[] args) {
+            Friend alphonse = new Friend("Alphonse");
+            Friend gaston = new Friend("Gaston");
+
+            new Thread(() -> alphonse.bow(gaston)).start();
+            new Thread(() -> gaston.bow(alphonse)).start();
+        }
+    }
+}
